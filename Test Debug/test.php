10c0b28c1fc2bc4fd7aa12e7e30e844b92256462
stikirana <stikirana@stikirana-acer>
2012-12-07 11:52:30 +0800

    Perbaiki master

--- Test Debug/test.php
+++ Test Debug/test.php
@@ -1,9 +1,9 @@
 <?php
 echo 'Ini edit file branch.';
-echo 'Ditambah belajar Git, sdh mulai ga bingung';
+echo 'Ditambah belajar Git, sdh mulai ga bingung 2';
 
 $myvariable = "This is my variable.";
-$myvar = "Variabel tambahan";
+$myvar = "Variabel tambahan 2";
 phpinfo();
 
 
